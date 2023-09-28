082ce2d3c8bb16e35dd2ab1d85b0f15f40ebe77c
Guilherme-Wolff <95699454+Guilherme-Wolff@users.noreply.github.com>
2023-09-27 22:23:45 -0300

    Update http.ts

--- src/http.ts
+++ src/http.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { Server ,Socket} from "socket.io";
 import http from 'http'
+import cors from "cors"
 const PORT: number = 3001
 
 const app = express();
+app.use(cors());
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
